a4019f6f0a5b80630dd701a6b9966d8ae8bc88b3
谭梓阳 <tan-zy21@mails.tsinghua.edu.cn>
2023-07-15 13:21:34 +0800

    7/15 afternoon update: add servo control

---
+++ Car_Control_Ahead/Controller.py
@@ -0,0 +1,51 @@
+import socket
+import time
+import keyboard
+
+def get_key():
+    event = keyboard.read_event()
+    return event.name
+
+
+
+def clinet_init(host_address, host_port):
+    #套接字接口
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #设置ip和端口
+    host = host_address
+    port = host_port
+
+    try:
+        mySocket.connect((host, port))
+        print("连接成功") ##连接到服务器
+    except :                           ##连接不成功，运行最初的ip
+        print ('连接不成功')
+    
+    return mySocket
+        
+        
+def send(mySocket, text, delay):
+        #发送消息
+    msg = text
+    #编码发送
+    mySocket.send(msg.encode("utf-8"))
+    
+    time.sleep(delay)
+    
+    if msg == "over":
+        mySocket.close()
+        exit()
+
+if __name__ == '__main__':
+    ip = '192.168.137.30'
+    port = 2222
+    mySocket = clinet_init(ip, port)
+
+    try:
+        while True:
+            text = get_key()
+            send(mySocket, text, 0.01)
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        send(mySocket, 'over', 0.01)
+        pass
---
+++ turn/servo_detect.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+Ldr = 6
+GPIO.setup(Ldr, GPIO.IN)
+servo = 23
+GPIO.setup(servo, GPIO.OUT)
+pwm_servo = GPIO.PWM(servo, 50)
+pwm_servo.start(0)
+
+
+
+
+
+def servo_detect():
+    while True:
+        for pos in range(181):
+            servo_pulse(pos)
+            sleep(0.0009)
+            if GPIO.input(Ldr) == 0:
+                return
+        for pos in reversed(range(181)):
+            servo_pulse(pos)
+            sleep(0.0009)
+            if GPIO.input(Ldr) == 0:
+                return
+                    
+
+
+
+
+def servo_pulse(myangle):
+	pulsewidth = (myangle*11)+500
+	GPIO.output(servo,GPIO.HIGH)
+	sleep(pulsewidth/1000000.0)
+	GPIO.output(servo,GPIO.LOW)
+	sleep(20.0/1000-pulsewidth/1000000.0)
+
+
+
+
+
+
+
+try:
+    while True:
+        global angle_0
+        servo_detect()
+        print("detect!")
+        sleep(0.5)
+except KeyboardInterrupt:
+    pass
