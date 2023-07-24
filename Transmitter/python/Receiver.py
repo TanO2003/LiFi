984de6af0cce236c1b53d31213e6799d7355067c
谭梓阳 <tan-zy21@mails.tsinghua.edu.cn>
2023-07-24 19:07:48 +0800

    7/24 2

--- Transmitter/python/Receiver.py
+++ Transmitter/python/Receiver.py
@@ -1,22 +1,37 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import re
+import numpy as np
+from functools import reduce
+import operator as op
+
+
+def Haming_decode(text):
+    bits = np.array(list(text), dtype=int)
+    position = reduce(op.xor, [i for i, bit in enumerate(bits) if bit])
+    bits[position] = str(int(not(int(bits[position]))))
+    return ''.join(str(bits[i]) for i in (3, 5, 6, 7))
+
 
 def bin_2_str(bin):
     # 二进制转换为字符串
+    
     messages=bin[:-10]
-    #print(messages)
-    position=messages.rfind('10101010')
-    messages=messages[position+8:]
-    print(messages)
-    #messages=re.findall('10101010(.*?)10101010',bin)
-    messages.replace('000001','00000')
-    string=''
-    bytes_list = re.findall('.{8}', messages)
-    for byte in bytes_list:
-        string += chr(int(byte, 2))
-    return string
-    #return ''.join([chr(i) for i in [int(b, 2) for b in bin.split('')]])
+    position=messages.rfind('100100100')
+    messages=messages[position+9:]
+    try:
+        messages_list=[Haming_decode(messages[i:i+8]) for i in range(0, len(messages), 8)]
+        messages=''.join(messages_list)
+        messages.replace('000001','00000')
+        string=''
+        bytes_list = re.findall('.{8}', messages)
+        for byte in bytes_list:
+            string += chr(int(byte, 2))
+        return string
+    except Exception:
+        print("Error！")
+        return -1
+
 
 GPIO.setmode(GPIO.BCM)
 
